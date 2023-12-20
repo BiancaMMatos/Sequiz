94b2254d2b26a070b639262f7a48a55c070de5af
BiancaMMatos <macielmatosb@gmail.com>
2023-12-20 20:45:11 -0300

    feature/collectionViewHome: Criando constraints da Collection

--- Sequiz/Sequiz/ViewController.swift
+++ Sequiz/Sequiz/ViewController.swift
@@ -6,20 +6,54 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
-    private var titleLabel = UILabel()
+    // MARK: - Properties
+    private var images: [UIImage] = []
     
+    // MARK: - UI Components
+    private var titleLabel = UILabel()
+    private let collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+       layout.scrollDirection = .vertical
+        
+       let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+       return collectionView
+    }()
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureUI()
+        
+        for _ in 0...6 {
+            images.append(UIImage(named: "Friends") ?? UIImage())
+            images.append(UIImage(named: "Game of Thrones") ?? UIImage())
+            images.append(UIImage(named: "How I Met You Mother") ?? UIImage())
+            images.append(UIImage(named: "Lost") ?? UIImage())
+            images.append(UIImage(named: "PLL") ?? UIImage())
+            images.append(UIImage(named: "The Bear") ?? UIImage())
+        }
+        
     }
 
-    override func loadView() {
-        let view = UIView(frame: UIScreen.main.bounds)
+
+    private func configureUI() {
         view.backgroundColor = .accent
-                self.view = view
+        
+        self.view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.snp.top).offset(0)
+            make.bottom.equalTo(self.view.snp.bottom).offset(0)
+            make.leading.equalTo(self.view.snp.leading).offset(0)
+            make.trailing.equalTo(self.view.snp.trailing).offset(0)
+        }
+        
     }
 
 }
