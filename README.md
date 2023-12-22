e76b617bfcb5b077bb272c6495d9216b411afdf0
BiancaMMatos <macielmatosb@gmail.com>
2023-12-22 16:26:52 -0300

    feature/collectionViewHome: Adicionando collection + Arrumando podfie

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sequiz
-App que permite com que o usuário mostre os seus conhecimentos sobre as séries
--- Sequiz/Pods/Target Support Files/Pods-SequizTests/Pods-SequizTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SequizTests : NSObject
-@end
-@implementation PodsDummy_Pods_SequizTests
-@end
